145f5a29d8691f029145cf7b00a30b233ad1af08
jklingstedt <jklingstedt@jawbone.com>
2016-12-20 14:48:08 -0800

    changes before everything breaks

--- app/containers/HomeContainer.js
+++ app/containers/HomeContainer.js
@@ -1,10 +1,10 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import Home from '../components/Home'
 
 class HomeContainer extends Component {
-  render () {
-    return <Home />
-  }
+    render() {
+        return <Home />
+    }
 };
 
 export default HomeContainer;
