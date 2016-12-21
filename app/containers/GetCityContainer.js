562d04961e3c16bb1e2cc6914badd8514ed44157
jklingstedt <jklingstedt@jawbone.com>
2016-12-20 17:01:22 -0800

    finished

--- app/containers/GetCityContainer.js
+++ app/containers/GetCityContainer.js
@@ -1,42 +1,48 @@
-var React = require('react');
-var PropTypes = React.PropTypes;
+import React, { PropTypes, Component } from 'react'
 import GetCity from '../components/GetCity'
 
-var GetCityContainer = React.createClass({
-  contextTypes: {
-    router: React.PropTypes.object.isRequired
-  },
-  getDefaultProps: function () {
-    return {
-      direction: 'column'
-    }
-  },
-  propTypes: {
-    direction: PropTypes.string
-  },
-  getInitialState: function () {
-    return {
-      city: ''
-    }
-  },
-  handleSubmitCity: function (e) {
-    e.preventDefault()
-    this.context.router.push('/forecast/' + this.state.city)
-  },
-  handleUpdateCity: function (e) {
-    this.setState({
-      city: e.target.value
-    })
-  },
-  render: function () {
-    return (
-      <GetCity
-        direction={this.props.direction}
-        onSubmitCity={this.handleSubmitCity}
-        onUpdateCity={this.handleUpdateCity}
-        city={this.state.city} />
-    )
-  }
-});
+class GetCityContainer extends Component {
+    constructor() {
+        super();
+        this.state = {
+            direction: 'column',
+            city: ''
+        }
+    };
 
-module.exports = GetCityContainer;
+    handleSubmitCity(e) {
+        e.preventDefault()
+        this.context.router.push('/forecast/' + this.state.city)
+    };
+
+    handleUpdateCity(e) {
+        this.setState({
+            city: e.target.value
+        })
+    };
+
+    render() {
+        return (
+            <GetCity
+                direction={this.props.direction}
+                onSubmitCity={(e) => this.handleSubmitCity(e)}
+                onUpdateCity={(e) => this.handleUpdateCity(e)}
+                city={this.state.city}
+                />
+        )
+    }
+};
+
+GetCityContainer.contextTypes = {
+    router: React.PropTypes.object.isRequired
+};
+
+GetCityContainer.propTypes = {
+    direction: PropTypes.string
+}
+
+GetCityContainer.defaultProps = {
+    direction: 'column'
+};
+
+export default GetCityContainer;
--- app/helpers/api.js
+++ app/helpers/api.js
@@ -1,49 +1,42 @@
-var axios = require('axios');
+import axios from 'axios'
 
-var _baseURL = 'http://api.openweathermap.org/data/2.5/';
-var _APIKEY = 'b714ec74bbab5650795063cb0fdf5fbe';
+const _baseURL = 'http://api.openweathermap.org/data/2.5/';
+const _APIKEY = 'b714ec74bbab5650795063cb0fdf5fbe';
 
-function prepRouteParams (queryStringData) {
-  return Object.keys(queryStringData)
-    .map(function (key) {
-      return key + '=' + encodeURIComponent(queryStringData[key]);
-    }).join('&')
+function prepRouteParams(queryStringData) {
+    return Object.keys(queryStringData)
+        .map((key) => {
+            return key + '=' + encodeURIComponent(queryStringData[key]);
+        }).join('&')
 }
 
-function prepUrl (type, queryStringData) {
-  return _baseURL + type + '?' + prepRouteParams(queryStringData);
+function prepUrl(type, queryStringData) {
+    return _baseURL + type + '?' + prepRouteParams(queryStringData);
 }
 
-function getQueryStringData (city) {
-  return {
-    q: city,
-    type: 'accurate',
-    APPID: _APIKEY,
-    cnt: 5
-  }
+function getQueryStringData(city) {
+    return {
+        q: city,
+        type: 'accurate',
+        APPID: _APIKEY,
+        cnt: 5
+    }
 }
 
-function getCurrentWeather (city) {
-  var queryStringData = getQueryStringData(city);
-  var url = prepUrl('weather', queryStringData)
+export function getCurrentWeather(city) {
+    const queryStringData = getQueryStringData(city);
+    const url = prepUrl('weather', queryStringData)
 
-  return axios.get(url)
-    .then(function (currentWeatherData) {
-      return currentWeatherData.data
-    })
+    return axios.get(url)
+        .then((currentWeatherData) => currentWeatherData.data)
 }
 
-function getForcast (city) {
-  var queryStringData = getQueryStringData(city);
-  var url = prepUrl('forecast/daily', queryStringData)
+export function getForcast(city) {
+    const queryStringData = getQueryStringData(city);
+    const url = prepUrl('forecast/daily', queryStringData)
 
-  return axios.get(url)
-    .then(function (forecastData) {
-      return forecastData.data
-    })
-}
-
-module.exports = {
-  getCurrentWeather: getCurrentWeather,
-  getForcast: getForcast
+    return axios.get(url)
+        .then((forecastData) => {
+            return forecastData.data
+        });
 };
